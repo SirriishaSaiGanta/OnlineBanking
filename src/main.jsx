1ff9348f60dd6f5d4f2912b2a81517c1e1d72133
Sirriisha <gantasaisirriisha@gmail.com>
2024-09-01 13:16:28 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GlobalState from "./Components/GlobalContext/Context.jsx";
 
 createRoot(document.getElementById("root")).render(
