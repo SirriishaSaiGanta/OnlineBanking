f2979f6558833251aa9faea0ac9a3b4b11ece6d0
Sirriisha <gantasaisirriisha@gmail.com>
2024-09-01 12:56:17 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -10,6 +10,5 @@ createRoot(document.getElementById("root")).render(
     <GlobalState>
       <App/>
     </GlobalState>
-
 
 );
