b00b777ac8e02f3884f2a38c1954f0315eac5f18
Sirriisha <gantasaisirriisha@gmail.com>
2024-09-01 13:29:23 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import App from "./App.jsx";
 import "./index.css";
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GlobalState from "./Components/GlobalContext/Context.jsx";
+import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
-  
+  <StrictMode>
     <GlobalState>
       <App/>
     </GlobalState>
-
+    </StrictMode>
 );
